dd1a35111300e9af271e4d81baf889c0c2503b1f
CayoPOliveira <cayoproliveira@gmail.com>
2026-01-05 14:46:20 -0300

    chore(Chatwoot): update Kanban step description for improved clarity

--- nodes/Chatwoot/Chatwoot.node.ts
+++ nodes/Chatwoot/Chatwoot.node.ts
@@ -124,7 +124,7 @@ export class Chatwoot implements INodeType {
 					{
 						name: 'Kanban Step',
 						value: 'kanbanStep',
-						description: 'Manage Kanban steps (columns)',
+						description: 'Manage Kanban steps',
 					},
 					{
 						name: 'Kanban Task',
